cda4edc62a148a040ff75324decc07867838f4c9
Christian-007 <christianingsunardi@gmail.com>
2024-11-11 16:45:57 +0700

    03-message-broker-03-subscribing

--- 03-message-broker/03-subscribing/main.go
+++ 03-message-broker/03-subscribing/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"os"
 
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-redisstream/pkg/redisstream"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	logger := watermill.NewStdLogger(false, false)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_ADDR"),
+	})
+
+	subscriber, err := redisstream.NewSubscriber(redisstream.SubscriberConfig{
+		Client: rdb,
+	}, logger)
+	if err != nil {
+		panic(err)
+	}
+
+	messages, err := subscriber.Subscribe(context.Background(), "progress")
+	if err != nil {
+		panic(err)
+	}
+
+	for message := range messages {
+		progress := string(message.Payload)
+		fmt.Printf("Message ID: %v - %v%%\n", message.UUID, progress)
+		message.Ack()
+	}
 }
