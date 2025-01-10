d691d630ef2bd5492305c61dcc519153ea865c9b
Christian Ing Sunardi <christianingsunardi@gmail.com>
2025-01-10 18:13:02 +0700

    08-component-testing-05-project-testing-ticket-confirmed

--- project/api/spreadsheet_mock.go
+++ project/api/spreadsheet_mock.go
@@ -2,10 +2,22 @@ package api
 
 import (
 	"context"
+	"sync"
 )
 
-type SpreadsheetsAPIMock struct{}
+type SpreadsheetsAPIMock struct{
+	lock sync.Mutex
+	Rows map[string][][]string
+}
 
-func (c SpreadsheetsAPIMock) AppendRow(ctx context.Context, spreadsheetName string, row []string) error {
+func (c *SpreadsheetsAPIMock) AppendRow(ctx context.Context, spreadsheetName string, row []string) error {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	if c.Rows == nil {
+		c.Rows = make(map[string][][]string)
+	}
+
+	c.Rows[spreadsheetName] = append(c.Rows[spreadsheetName], row)
 	return nil
 }
